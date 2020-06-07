9ea8a3c08510ce7304c6990ad79b8f3d29b1cd80
Jigyasu Prakash <jigyasuprakash3@gmail.com>
2020-06-07 19:00:57 +0530

    deploy ready

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -10,123 +10,123 @@ class Preview extends Component {
     constructor(props) {
         super(props)
 
-        // this.state = {
-        //     tower: this.props.location.tower,
-        //     uniqueArea: this.props.location.uniqueArea,
-        //     uniqueAtt: this.props.location.uniqueAtt,
-        //     flatTypes: this.props.location.flatTypes,
-        //     facing: this.props.location.facing,
-        //     page: 1
-        // }
-
         this.state = {
-            tower: [
-                {
-                    tid: 'T1',
-                    tname: 'ABC',
-                    floors: [
-                        {
-                            fid: 'T1F1',
-                            floor_no: 1,
-                            units: [
-                                {
-                                    uid: 'T1F1U1',
-                                    bhk_type: 0,
-                                    size: 0
-                                },
-                                {
-                                    uid: 'T1F1U2',
-                                    bhk_type: 0,
-                                    size: 0
-                                },
-                                {
-                                    uid: 'T1F1U3',
-                                    bhk_type: 0,
-                                    size: 0
-                                },
-                                {
-                                    uid: 'T1F1U4',
-                                    bhk_type: 0,
-                                    size: 0
-                                },
-                                {
-                                    uid: 'T1F1U5',
-                                    bhk_type: 0,
-                                    size: 0
-                                }
-                            ]
-                        },
-                        {
-                            fid: 'T1F2',
-                            floor_no: 2,
-                            units: [
-                                {
-                                    uid: 'T1F2U1',
-                                    bhk_type: 0,
-                                    size: 0
-                                },
-                                {
-                                    uid: 'T1F2U2',
-                                    bhk_type: 0,
-                                    size: 0
-                                },
-                                {
-                                    uid: 'T1F2U3',
-                                    bhk_type: 0,
-                                    size: 0
-                                },
-                                {
-                                    uid: 'T1F2U4',
-                                    bhk_type: 0,
-                                    size: 0
-                                },
-                                {
-                                    uid: 'T1F2U5',
-                                    bhk_type: 0,
-                                    size: 0
-                                }
-                            ]
-                        },
-                        {
-                            fid: 'T1F3',
-                            floor_no: 3,
-                            units: [
-                                {
-                                    uid: 'T1F3U1',
-                                    bhk_type: 0,
-                                    size: 0
-                                },
-                                {
-                                    uid: 'T1F3U2',
-                                    bhk_type: 0,
-                                    size: 0
-                                },
-                                {
-                                    uid: 'T1F3U3',
-                                    bhk_type: 0,
-                                    size: 0
-                                },
-                                {
-                                    uid: 'T1F3U4',
-                                    bhk_type: 0,
-                                    size: 0
-                                },
-                                {
-                                    uid: 'T1F3U5',
-                                    bhk_type: 0,
-                                    size: 0
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ],
-            uniqueArea: [{ key: 1, label: 1025 }, { key: 2, label: 1300 }],
-            uniqueAtt: [{ key: 1, label: "Garden Facing" }, { key: 2, label: "Club House Facing" }],
-            flatTypes: [2, 2.5, 3],
-            facing: ['East', 'West', 'North'],
+            tower: this.props.location.tower,
+            uniqueArea: this.props.location.uniqueArea,
+            uniqueAtt: this.props.location.uniqueAtt,
+            flatTypes: this.props.location.flatTypes,
+            facing: this.props.location.facing,
             page: 1
         }
+
+        // this.state = {
+        //     tower: [
+        //         {
+        //             tid: 'T1',
+        //             tname: 'ABC',
+        //             floors: [
+        //                 {
+        //                     fid: 'T1F1',
+        //                     floor_no: 1,
+        //                     units: [
+        //                         {
+        //                             uid: 'T1F1U1',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         },
+        //                         {
+        //                             uid: 'T1F1U2',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         },
+        //                         {
+        //                             uid: 'T1F1U3',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         },
+        //                         {
+        //                             uid: 'T1F1U4',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         },
+        //                         {
+        //                             uid: 'T1F1U5',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         }
+        //                     ]
+        //                 },
+        //                 {
+        //                     fid: 'T1F2',
+        //                     floor_no: 2,
+        //                     units: [
+        //                         {
+        //                             uid: 'T1F2U1',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         },
+        //                         {
+        //                             uid: 'T1F2U2',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         },
+        //                         {
+        //                             uid: 'T1F2U3',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         },
+        //                         {
+        //                             uid: 'T1F2U4',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         },
+        //                         {
+        //                             uid: 'T1F2U5',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         }
+        //                     ]
+        //                 },
+        //                 {
+        //                     fid: 'T1F3',
+        //                     floor_no: 3,
+        //                     units: [
+        //                         {
+        //                             uid: 'T1F3U1',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         },
+        //                         {
+        //                             uid: 'T1F3U2',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         },
+        //                         {
+        //                             uid: 'T1F3U3',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         },
+        //                         {
+        //                             uid: 'T1F3U4',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         },
+        //                         {
+        //                             uid: 'T1F3U5',
+        //                             bhk_type: 0,
+        //                             size: 0
+        //                         }
+        //                     ]
+        //                 }
+        //             ]
+        //         }
+        //     ],
+        //     uniqueArea: [{ key: 1, label: 1025 }, { key: 2, label: 1300 }],
+        //     uniqueAtt: [{ key: 1, label: "Garden Facing" }, { key: 2, label: "Club House Facing" }],
+        //     flatTypes: [2, 2.5, 3],
+        //     facing: ['East', 'West', 'North'],
+        //     page: 1
+        // }
     }
 
     render() {
