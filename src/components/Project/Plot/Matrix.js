caf86da9760b472aa8999628181b0367bed4a7bf
Jigyasu Prakash <jigyasuprakash3@gmail.com>
2020-06-28 16:06:42 +0530

    v1.0.2

--- src/components/Project/Plot/Matrix.js
+++ src/components/Project/Plot/Matrix.js
@@ -65,7 +65,7 @@ function PreviewMatrix({ tower, filter, mySelect, selected }) {
             <TableContainer>
                 <Typography variant="h5" style={{ borderBottom: "1px lightgrey solid", margin: "10px" }} align="center">{tower.bname}</Typography>
                 <Table size="small" aria-label="simple table">
-                    <TableBody>{tower.floors[0].floor_no < (tower.floors === undefined ? 0 : tower.floors[1].floor_no) ? (
+                    <TableBody>{tower.floors[0].fid.split('F')[1] < (tower.floors[1] === undefined ? 0 : tower.floors[1].fid.split('F')[1]) ? (
                         tower.floors.reverse().map((floor) => (
                             <React.Fragment>
                                 <TableRow key={floor.fid}>
